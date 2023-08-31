42cec0faced53ab9ca506ee880a45b2209826115
zulhusnihariz <zulhusnihariz97@gmail.com>
2023-09-01 00:43:11 +0800

    fix: entity too large - replace bodyParser to express.json

--- index.ts
+++ index.ts
@@ -25,7 +25,9 @@ for (let i = 0; i < methods.length; i++) {
 
 const app: Express = express();
 
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+
 app.use(
   cors({
     origin: '*',
